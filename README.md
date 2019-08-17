d8d0eeedf2de673edaf74f87f56c417b11df23b2
Dawid Potocki <dpot@disroot.org>
2019-08-18 05:21:07 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# biual-firefox
+
+* Firefox
+	* [ghacks-user.js](https://github.com/ghacksuserjs/ghacks-user.js)
+	* Dark theme
+* Firefox Developer Edition
+	* Disabled telemetry
+
+## Install
+
+```
+$ git clone https://github.com/dawidpotocki/biual-firefox.git ~/.mozilla/firefox
+```
---
+++ biual.default/user-overrides.js
@@ -0,0 +1,6 @@
+user_pref("keyword.enabled", false);
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
+user_pref("browser.search.hiddenOneOffs", "Google,Bing,Amazon.com,DuckDuckGo,Twitter,Wikipedia (en)");
+user_pref("lightweightThemes.selectedThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("devtools.theme", "dark");
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"library-button\",\"sidebar-button\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":16,\"newElementCount\":3}");
